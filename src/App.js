04a3879534575e76f300ba328b6017f4022bf11c
Pedro Barrera <fiticostini@users.noreply.github.com.>
2023-09-29 14:18:07 -0400

    pagina register

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+
 
 function App() {
   return (
     <>
       <TopBar />
-      <Settings />
+      <Register />
     </>
   );
 }
