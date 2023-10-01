d67e14710d9f861caeaff22e1007be76c3941080
Pedro Barrera <fiticostini@users.noreply.github.com.>
2023-10-01 16:11:31 -0400

    react-router-dom quiebre

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <TopBar />
-      <Register />
-    </>
+      <Routes>
+        <Route path="/"> element={<Home />} </Route>
+      
+      </Routes> 
+    </BrowserRouter>
   );
 }
 
